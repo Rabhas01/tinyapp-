c880c12a2e93c61e8b529e9f7d6e4b8d71b359bb
Rabhas <rabhas.vashisht@gmail.com>
2022-03-22 03:27:01 +0000

    shortURL-longURL key-value pair are saved to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -9,10 +9,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
+
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -35,10 +32,16 @@ app.get("/urls/:shortURL",(req, res) => {
   res.render("urls_show", templateVars)
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL
+});
+
 function generateRandomString() {
   let result = '';
   let characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' 
-  for (let i = 0; i < 5 ; i++ ){
+  for (let i = 0; i < 6 ; i++ ){
   result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
